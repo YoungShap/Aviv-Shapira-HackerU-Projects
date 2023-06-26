0e2969d4ab0d77b48be75172069aa48f52df9861
Aviv <avivshapira22@gmail.com>
2023-06-26 16:00:00 +0300

    update

---
+++ JS-TS-Projects/project3/script/main.ts
@@ -0,0 +1,178 @@
+interface Task {
+    id: number;
+    title: string;
+    description: string;
+    addedTime: string;
+    priority: PriorityTypes;
+    isCompleted: boolean;
+}
+
+enum PriorityTypes {
+    low,
+    medium,
+    high,
+}
+
+class TaskManager {
+    tasks: Task[] = [
+        {
+            id: 1,
+            title: 'דוגמא',
+            addedTime: '2023-06-11 11:11:22',
+            description: 'פה יופיע תיאור המשימה',
+            isCompleted: false,
+            priority: PriorityTypes.low,
+        },
+        {
+            id: 2,
+            title: 'דוגמא',
+            addedTime: '2023-06-11 11:11:22',
+            description: 'פה יופיע תיאור המשימה',
+            isCompleted: false,
+            priority: PriorityTypes.high,
+        },
+        {
+            id: 3,
+            title: 'דוגמא',
+            addedTime: '2023-06-11 11:11:22',
+            description: 'פה יופיע תיאור המשימה',
+            isCompleted: false,
+            priority: PriorityTypes.medium,
+        },
+    ];
+
+    constructor() {
+        this.showTasks();
+
+        const elem = document.querySelector("header");
+
+        // מגדירים שבלחיצה על הכפתור תופעל פונקציה המוסיפה משימה
+        elem?.querySelector("button")?.addEventListener("click", ev => {
+            const elemTitle = elem?.querySelector("input");
+            const elemPriority = elem?.querySelector("select");
+            const elemdescription = elem?.querySelector("input.dis") as HTMLInputElement;
+
+            const title = elemTitle?.value || '';
+            const priority = elemPriority?.value || '';
+            const description = elemdescription?.value || '';
+
+            // איפוס התיבה של הכותרת
+            if (elemTitle) {
+                elemTitle.value = "";
+            }
+
+            // איפוס התיבה של רמת העדיפות
+            if (elemPriority) {
+                elemPriority.value = "";
+            }
+            if (elemdescription) {
+                elemdescription.value = "";
+            }
+
+            this.addTask(title, +priority, description);
+        });
+    }
+
+    addTask(title: string, priority?: PriorityTypes, description?: string) {
+        // מערך של ה-ids
+        const ids = this.tasks.map(x => x.id);
+        const max = Math.max(...ids);
+
+        const now = new Date();
+        const y = now.getFullYear();
+        const m = now.getMonth() + 1;
+        const d = now.getDate();
+
+        const h = now.getHours();
+        const mn = now.getMinutes();
+        const s = now.getSeconds();
+
+        const addedTime = `${y}-${(m < 10 ? '0' + m : m)}-${d} ${h}:${mn}:${s}`;
+
+        this.tasks.push({
+            id: max + 1,
+            title,
+            addedTime,
+            description:`${description}`,
+            isCompleted: false,
+            priority: priority || PriorityTypes.low,
+        });
+
+        this.showTasks();
+    }
+
+    editTask(taskId: number) {
+    
+    }
+
+    removeTask(taskId: number) {
+        const i = this.tasks.findIndex(x => x.id == taskId);
+        this.tasks.splice(i, 1);
+
+        this.showTasks();
+    }
+
+    completeTask(taskId: number) {
+        const item = this.tasks.find(x => x.id == taskId);
+
+        if (item) {
+            item.isCompleted = true;
+        }
+
+        this.showTasks();
+    }
+
+    unCompleteTask(taskId: number) {
+        const item = this.tasks.find(x => x.id == taskId);
+
+        if (item) {
+            item.isCompleted = false;
+        }
+
+        this.showTasks();
+    }
+
+    showTasks() {
+        const elem = document.querySelector("div.tasks");
+       
+        
+        if (elem) {
+            elem.innerHTML = "";
+        }
+
+        this.tasks.forEach(t => {
+            const div = document.createElement("div");
+
+            if (t.isCompleted) {
+                div.classList.add('completed');
+            }
+
+            switch (t.priority) {
+                case PriorityTypes.low : div.classList.add('low'); break;
+                case PriorityTypes.medium : div.classList.add('medium'); break;
+                case PriorityTypes.high : div.classList.add('high'); break;
+            }
+
+            div.innerHTML = `
+                <h3>${t.title}</h3>
+                <p><b>זמן יצירה:</b> ${t.addedTime}</p>
+                <p><b>תיאור:</b> ${t.description || '*אין הערה*'}</p>
+
+                <footer>
+                    <button class="remove">מחק</button>
+                    ${t.isCompleted ? '<button class="uncomplete">לא בוצע</button>' : '<button class="complete">בוצע</button>'}
+                    <button class="edit">עריכה</button>
+                </footer>
+            `;
+
+            div.querySelector('.remove')?.addEventListener("click", () => this.removeTask(t.id));
+            div.querySelector('.complete')?.addEventListener("click", () => this.completeTask(t.id));
+            div.querySelector('.uncomplete')?.addEventListener("click", () => this.unCompleteTask(t.id));
+            div.querySelector('.edit')?.addEventListener("click", () => this.editTask(t.id));
+
+            elem?.appendChild(div);
+        });
+    }
+}
+
+const tasks = new TaskManager;
---
+++ JS-TS-Projects/project4/script/script.js
@@ -0,0 +1,114 @@
+const elem = document.querySelector(".board");
+let isX = true;
+let winner;
+let winnerArray;
+let scoreX = localStorage.getItem("scoreX") || 0;
+let scoreO = localStorage.getItem("scoreO") || 0;
+
+function create() {
+    for (let i = 0; i < 9; i++) {
+        const div = document.createElement('div');
+        document.querySelector('.scoreX').innerHTML = scoreX;
+        document.querySelector('.scoreO').innerHTML = scoreO;
+        div.addEventListener('click', ev => {
+            const clickedDiv = ev.target;
+
+            if (!clickedDiv.innerHTML && !winner) {
+                if (isX) {
+                    clickedDiv.innerHTML = 'X';
+                } else {
+                    clickedDiv.innerHTML = "O"
+                }
+                clickedDiv.className = 'dirty';
+                isX = !isX;
+                check();
+            }
+        })
+        elem.appendChild(div);
+    }
+}
+
+create();
+
+function check() {
+    const divs = elem.querySelectorAll('div');
+
+    const options = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (const arr of options) {
+        const res = arr.map(index => divs[index].innerHTML);
+
+        if (res.every(val => val === 'X')) {
+            winner = 'X';
+            localStorage.setItem('scoreX', ++scoreX);
+            document.querySelector('.scoreX').innerHTML = scoreX;
+            winnerArray = arr;
+            break;
+        } else if (res.every(val => val === 'O')) {
+            winner = 'O';
+            localStorage.setItem('scoreO', ++scoreO)
+           document.querySelector('.scoreO').innerHTML = scoreO;
+
+            winnerArray = arr;
+            break;
+        }
+    }
+
+    if (winner) {
+        elem.querySelectorAll('div').forEach(el => {
+            el.className = 'dirty';
+        });
+
+        winnerArray.forEach(index => divs[index].classList.add('bg'));
+
+        showWinner(`The winner is ${winner}`);
+    }
+}
+
+function showWinner(text) {
+    const winner = document.createElement("div");
+    winner.classList.add("winner");
+    winner.innerHTML = text;
+
+    const frame = document.querySelector(".frame");
+    frame.appendChild(winner);
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        decay: 0.9,
+        origin: { y: 0.6 }
+    });
+    
+    setTimeout(function() {
+        frame.removeChild(winner);
+    }, 2 * 1000);
+}
+function reset() {
+    elem.innerHTML = "";
+    create();
+    winner = false;
+}
+
+function resetScore() {
+    localStorage.clear();
+    location.reload();
+}
+
+// להציג כל פעם מחווה של מי התור הנוכחי
+// לאפשר לצבור נקודות
+// כפתור איפוס
+// המנצח הוא הראשון בפעם הבאה
+
+
+// אתגר!
+// לאפשר טיימר למשחק
